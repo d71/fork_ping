cce04f1df36c68be2969ea01849115b795070588
d71 <fddkiller@mail.ru>
2014-12-04 18:04:49 +0900

    add conf file for make_graph

--- make_graph/make_gd_graph.c
+++ make_graph/make_gd_graph.c
@@ -13,12 +13,23 @@
 #include <gd.h>
 #include <gdfontg.h>
 
-int main(void)
+#define BUF_SIZE 256
+
+char server[BUF_SIZE];
+char user[BUF_SIZE];
+char password[BUF_SIZE];
+char database[BUF_SIZE];
+char table_hosts[BUF_SIZE];
+char table_timeline[BUF_SIZE];
+char graph_dir[BUF_SIZE];
+
+int main(int argc, char * argv[])
 {
 
     time_t time_start,time_end; //для измерения времени исполнения
     time_start = time(NULL);
 
+    read_config(argv[1]);
 
     gdImagePtr im1;
     FILE *out;
@@ -32,10 +43,10 @@ int main(void)
     MYSQL *conn;
     MYSQL_RES *res,*res_ip;
     MYSQL_ROW row, row_ip;
-    char *server = "localhost";
-    char *user = "ping";
-    char *password = "mypingtest";
-    char *database = "ping";
+//    char *server = "localhost";
+//    char *user = "ping";
+//    char *password = "mypingtest";
+//    char *database = "ping";
    
     int live=0;
     char query_string[256];
@@ -57,18 +68,18 @@ int main(void)
     char string_ip[256],file_name[256];
     int life;
 
-    mysql_query(conn,"select ip from hosts order by ip");
+    sprintf(query_string,"select ip from %s order by ip",table_hosts);
+
+    mysql_query(conn,query_string);
     res = mysql_store_result(conn);
 
     while( row=mysql_fetch_row(res) ){
 	
 	host=row[0];
 
-	//printf("work w host=%s\n",host);
-
 	memset( string_ip,0,sizeof(string_ip) );
     
-	sprintf(string_ip,"select ping,time1 from timeline WHERE host='%s' order by time1 DESC LIMIT 288",host);
+	sprintf(string_ip,"select ping,time1 from %s WHERE host='%s' order by time1 DESC LIMIT 288",table_timeline,host);
 	mysql_query(conn,string_ip);
 	res_ip = mysql_store_result(conn);
 
@@ -86,8 +97,6 @@ int main(void)
 	    if( strstr(row_ip[1],"00:00:")==0 ){
 		gdImageLine(im1,i,10,i,10-(life*10),green);
 	    }else{
-		//printf("zero\n");
-
 		gdImageLine(im1,i,10,i,10-(life*10),dark_green);
 	    }
 	    
@@ -96,8 +105,9 @@ int main(void)
 	    
 	}
 
-
-	sprintf(file_name,"/var/www/html/ping/graph/%s.png",host);
+	printf("%s/%s\n",graph_dir,host);
+	
+	sprintf(file_name,"%s/%s.png",graph_dir,host);
 	out = fopen(file_name, "wb");
 	gdImagePng(im1, out);
 	fclose(out);
@@ -115,4 +125,50 @@ int main(void)
 
 
     return 0;
+}
+
+int read_config (char *config_file) { 
+
+    FILE *file;
+    
+    int N=256;
+    
+    char arr[N];
+ 
+    file = fopen(config_file, "r");
+ 
+    while (fgets (arr, N, file) != NULL){
+
+	arr[strlen(arr)-1]='\0';
+
+	if(strstr(arr,"=")!=0){
+	    
+	    
+	    // разделяем строку пришедшую на команды и параметры, разделитель - пробел
+	    char cmd_line[2][50];
+	    int cmd_i=0;
+	    char *cmd_tmp, *last;
+	    
+	    cmd_tmp=strtok_r(arr, "=",&last);
+
+	    while(cmd_tmp!=NULL){
+		strcpy(cmd_line[cmd_i],cmd_tmp);
+		cmd_i++;
+		cmd_tmp = strtok_r(NULL,"=",&last);
+	    }
+
+	    if( strstr(cmd_line[0],"db_host") )		strcpy(server,cmd_line[1]);
+	    if( strstr(cmd_line[0],"db_user") )		strcpy(user,cmd_line[1]);
+	    if( strstr(cmd_line[0],"db_password") )	strcpy(password,cmd_line[1]);
+	    if( strstr(cmd_line[0],"db_name") )		strcpy(database,cmd_line[1]);
+	    if( strstr(cmd_line[0],"table_hosts") )	strcpy(table_hosts,cmd_line[1]);
+	    if( strstr(cmd_line[0],"table_timeline") )	strcpy(table_timeline,cmd_line[1]);
+	    if( strstr(cmd_line[0],"graph_dir") )	strcpy(graph_dir,cmd_line[1]);
+	
+	}
+    }
+    
+    fclose(file);
+
+    return;
 }
